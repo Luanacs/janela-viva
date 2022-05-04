6614f29b4f6ec7e403eeafe37694be4b6fb82a5d
ArethaQ <samara.7010b@gmail.com>
2022-05-04 10:07:14 -0300

    feat: crud completo EventoController

--- backend/src/main/java/com/janelaviva/janelaviva/controller/EventoController.java
+++ backend/src/main/java/com/janelaviva/janelaviva/controller/EventoController.java
@@ -1,6 +1,7 @@
 package com.janelaviva.janelaviva.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,42 +9,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.janelaviva.janelaviva.model.Evento;
 import com.janelaviva.janelaviva.repository.EventoRepository;
 
 @RestController
-@RequestMapping("/postagens")
+@RequestMapping("/eventos")
 @CrossOrigin("*")
 public class EventoController {
-	
-		@Autowired
-		private EventoRepository repository;
-		
-		@GetMapping
-		public ResponseEntity<List<Evento>> getAll(){
-			return ResponseEntity.ok(repository.findAll());
-		}
-		
-		@GetMapping("/{id}")
-		public ResponseEntity<Evento>getById(@PathVariable Long id) {
-			return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		}
-		
-//		@PostMapping
-//		public ResponseEntity<Evento>post(@Valid @RequestBody Evento evento) {
-//			return ResponseEntity.ok(repository.
-//		}
-	
-	
-	
-	
+
+	@Autowired
+	private EventoRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Evento>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Evento> getById(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Evento> post(@Valid @RequestBody Evento evento) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(evento));
+	}
+
+	@PutMapping
+	public ResponseEntity<Evento> put(@Valid @RequestBody Evento evento) {
+		return repository.findById(evento.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(repository.save(evento)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Evento> evento = repository.findById(id);
+
+		if (evento.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		repository.deleteById(id);
+	}
 
 }
