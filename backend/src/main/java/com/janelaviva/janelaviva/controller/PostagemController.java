24c705b9bc2106ce2055f06c1e0873cf201328d5
jandersonpaixao <janderson.paixao.a@gmail.com>
2022-05-06 10:35:52 -0300

    FEAT: Criando CRUD Completo do PostagemController

--- backend/src/main/java/com/janelaviva/janelaviva/controller/PostagemController.java
+++ backend/src/main/java/com/janelaviva/janelaviva/controller/PostagemController.java
@@ -1,6 +1,7 @@
 package com.janelaviva.janelaviva.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,14 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.janelaviva.janelaviva.model.Evento;
 import com.janelaviva.janelaviva.model.Postagem;
+import com.janelaviva.janelaviva.repository.EventoRepository;
 import com.janelaviva.janelaviva.repository.PostagemRepository;
 
 @RestController
@@ -25,6 +32,9 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
+	
+	@Autowired
+	private EventoRepository eventoRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -39,6 +49,34 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem>post(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.ok().body(postagem);
+		boolean existeEvento = eventoRepository.existsById(postagem.getEvento().getId());
+		if(existeEvento == true) {
+			postagem = repository.save(postagem);
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagem);
+		}
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();		
 	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		if(repository.existsById(postagem.getId())) {
+			if(eventoRepository.existsById(postagem.getEvento().getId())) {
+				return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+			}
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = repository.findById(id);
+		
+		if(postagem.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		repository.deleteById(id);
+		
+	}	
 }
